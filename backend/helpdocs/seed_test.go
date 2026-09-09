73fa31dc646f1331346cf232e4b1a58e3abb6dc5
guofeng.su <guofeng.su@westwell-lab.com>
2026-09-09 20:03:37 +0800

    test: align embedded help document count

--- backend/helpdocs/seed_test.go
+++ backend/helpdocs/seed_test.go
@@ -7,7 +7,7 @@ func TestEmbeddedDocumentsAreValidStableAndIndependent(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(docs) != 25 {
+	if len(docs) != 24 {
 		t.Fatalf("got %d seeded docs", len(docs))
 	}
 	seen := map[string]bool{}
